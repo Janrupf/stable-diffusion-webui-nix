94acb391bdd733a50aeb8dd7b4fc469256ca1ad5
Janrupf <business.janrupf@gmail.com>
2024-04-02 21:22:07 +0200

    Add workaround for dotfiles

--- package.nix
+++ package.nix
@@ -17,11 +17,18 @@ pkgs.writeShellScriptBin "stable-diffusion-webui" ''
   done
 
   if [[ -n "$dataDirOverride" ]]; then
-    echo "Automatically overriden data directory to '$dataDirOverride'"
-    prog_args+=("--data-dir" "$dataDirOverride")
+    echo "Automatically overwriting data directory to '$dataDirOverride'"
+
+    # Work around issues with gradio not allowing to serve dotfiles
+    tmp_dir="$(mktemp -d)"
+    link_dir="$tmp_dir/stable-diffusion-webui"
+
+    mkdir -p "$dataDirOverride"
+    ln -sf "$dataDirOverride" "$link_dir"
+
+    prog_args+=("--data-dir" "$link_dir")
   fi
 
-  # exec ${stable-diffusion-webui-python}/bin/python
   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver-32/lib"
 
   cd ${stable-diffusion-webui-git}
